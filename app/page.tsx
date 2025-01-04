d360fa1d1c6f97ea1ab38eb936099d9fa8f66646
mikami-haruhi <ktc24a31e0014@edu.kyoto-tech.ac.jp>
2025-01-04 10:36:17 +0900

    2024-01-04

--- app/page.tsx
+++ app/page.tsx
@@ -45,6 +45,7 @@ contents: News[]} = {
 };
 
 export default function Home(){
+  const sliceData = data.contents.slice(0,2);
   return(
     <>
     <section className={styles.top}>
@@ -63,6 +64,35 @@ export default function Home(){
     <section className={styles.news}>
       <h2 className={styles.newsTitle}>News</h2>
       <ul>
+        {sliceData.map((article) => (
+          <li key={article.id} className={styles.list}>
+            <div className={styles.link}>
+            <Image
+            className={styles.image}
+            src="/no-image.png"
+            alt="No Image"
+            width={1200}
+            height={630}
+            />
+            <dl className={styles.content}>
+              <dt className={styles.newsItemTitle}>{article.title}</dt>
+              <dd className={styles.meta}>
+                <span className={styles.tag}>{article.category.name}</span>
+                <span className={styles.date}>
+                  <Image
+                  src="/clock.svg"
+                  alt=""
+                  width={16}
+                  height={16}
+                  priority
+                  />
+                  {article.publishedAt}
+                  </span>
+              </dd>
+            </dl>
+          </div>
+          </li>
+        ))}
       </ul>
     </section>
     </>
